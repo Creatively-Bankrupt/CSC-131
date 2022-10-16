a5dec289b4cecfb112b4691962f4bfceabe4ee4c
Puth Vang <35241506+PuthVang@users.noreply.github.com>
2022-10-16 15:18:41 -0700

    Repository fix
    The repository should be arranged correctly and fixed now.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from "react";
-import Root from "./Root";
 import Other from "./Other";
 import TextField from "./TextField";
+import Vendia from "./Vendia";
+import { Grid } from "@mui/material";
+import CardsDMV from "./CardsDMV";
+import CardsSSN from "./CardsSSN";
+import CardsDOS from "./CardsDOS";
 
 function App() {
   return (
     <>
-    <Other />
-    <Root />
-    <TextField />
+    <Grid container spacing={2}>
+
+    <Grid xs={12}>
+      <Other />  {/** Travel X Logo */}
+    </Grid>
+
+    <Grid xs={12}>
+      <TextField /> {/** User Input Box */}
+    </Grid>
+
+    <Grid xs={12}>
+      <Vendia /> {/** Vendia List 1st person and Vendia Add Test User */}
+    </Grid>
+
+    <Grid xs={4}>
+      <CardsDMV />
+    </Grid>
+
+    <Grid xs={4}>
+      <CardsSSN />
+    </Grid>
+    
+    <Grid xs={4}>
+      <CardsDOS />
+    </Grid>
+
+    </Grid>
     </>
   );
 }
-
 export default App;
