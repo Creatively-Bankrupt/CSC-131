fc139bd82003e1117e4483b8f6422052bb7121ab
Puth Vang <35241506+PuthVang@users.noreply.github.com>
2022-10-18 10:47:23 -0700

    Updated and added some stuff
    Added
    - Installation batch file to make installing easier
    - Start batch file to start the website if needed
    
    Updated
    - MUI to v5.10.10
    
    Fixed
    - React-Scripts not working properly in package.json

---
+++ start.cmd
@@ -0,0 +1,10 @@
+@echo off
+cls
+for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
+
+echo If you're getting an error like this: '%ESC%[31mreact-scripts%ESC%[0m'%ESC%[31m is not recognized as an internal or external command%ESC% %ESC%[0m
+echo then you're missing some dependencies.
+echo You can run the file '%ESC%[32minstall.cmd%ESC% %ESC%[0m' to install the dependencies
+
+call npm start
+pause
