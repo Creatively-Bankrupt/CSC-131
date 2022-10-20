3d4e264ee3ba000898f06db1a0a805a95f0222f7
stef-aguilar <115608025+stef-aguilar@users.noreply.github.com>
2022-10-19 23:59:46 -0700

    Create App.js

---
+++ updated src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Other from "./Other";
+import TextField from "./TextField";
+import Vendia from "./Vendia";
+import { Grid } from "@mui/material";
+import CardsDMV from "./CardsDMV";
+import CardsSSN from "./CardsSSN";
+import CardsDOS from "./CardsDOS";
+
+function App() {
+  return (
+    
+    <>
+    <Grid container spacing={2}>
+
+    <Grid xs={12}>
+      <Other />  {/** Travel X Logo */}
+    </Grid>
+
+    <Grid xs={12}>
+      <TextField /> {/** User Input Box */}
+    </Grid>
+
+    <Grid xs={12}>
+      <Vendia /> {/** Vendia List 1st person and Vendia Add Test User */}
+    </Grid>
+
+    <Grid xs={4}>
+      <CardsDMV />
+    </Grid>
+
+    <Grid xs={4}>
+      <CardsSSN />
+    </Grid>
+    
+    <Grid xs={4}>
+      <CardsDOS />
+    </Grid>
+
+    </Grid>
+    </>
+  );
+}
+export default App;
