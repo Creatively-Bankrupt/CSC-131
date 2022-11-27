6a038a5ba0e4274107ff71b3636a312da3dc50ff
Nolendo <87155479+Nolendo@users.noreply.github.com>
2022-11-26 20:52:50 -0800

    11/26
    fixed my code regarding images to fit with the updated 3-uni code

--- App.js
+++ App.js
@@ -16,21 +16,41 @@ class App extends React.Component {
       data: '',
       file: '',
       type: [ 'dmv' ],
+      dobValue: '',
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleChangeDOB = this.handleChangeDOB.bind(this);
     this.handleClickShowPassword  = this.handleClickShowPassword.bind(this);
     this.handleMouseDownPassword = this.handleMouseDownPassword.bind(this);
     this.setData = this.setData.bind(this);
     this.setType = this.setType.bind(this);
     this.setFile = this.setFile.bind(this);
+    this.setDataSS = this.setDataSS.bind(this);
+    this.setDataDOS = this.setDataDOS.bind(this);
+    this.setDataDMV = this.setDataDMV.bind(this);
   }
 
   setType (newType) {
     this.setState({type: newType});
   }
-  
+  // get unparsed data
   setData (newData){
     this.setState({data: newData});
+  }
+  // get data from DMV search
+  setDataDMV (newData){
+    this.setState({dataDMV: newData});
+    console.log(this.state.dataDMV);
+  }
+  // get data from DMV search
+  setDataSS (newData){
+    this.setState({dataSS: newData});
+    console.log(this.state.dataSS);
+  }
+  // get data from DMV search
+  setDataDOS (newData){
+    this.setState({dataDOS: newData});
+    console.log(this.state.dataDOS);
   }
 
   setFile (newFile){
@@ -40,6 +60,10 @@ class App extends React.Component {
   handleChange (event) {
     const value = event.target.value;
     this.setState({value: formatSSN(event.target.value)});
+  }
+  handleChangeDOB (event) {
+    const dobValue = event.target.value;
+    this.setState({dobValue: event.target.value});
   }
 
   handleClickShowPassword (event) {
@@ -66,6 +90,7 @@ class App extends React.Component {
               password={this.state.value}
               showPassword={this.state.showPassword}
               handleChange={this.handleChange}
+              handleChangeDOB={this.handleChangeDOB}
               handleClickShowPassword={this.handleClickShowPassword}
               handleMouseDownPassword={this.handleMouseDownPassword}
             ></UserInput>
@@ -81,21 +106,21 @@ class App extends React.Component {
           <Buttons value={this.state.type} setType={this.setType}></Buttons>
         </Grid>
 
-        <Vendia ssn={this.state.value} setData={this.setData} setFile={this.setFile}></Vendia>
-
+        <Vendia ssn={this.state.value} setDataDMV={this.setDataDMV} setDataSS={this.setDataSS} setDataDOS={this.setDataDOS} setFile={this.setFile}></Vendia>
+        
         <Grid container item justifyContent="center" alignItems="center" gap={2}>
 
-            {/* { (this.state.data != null && this.state.data.items != null && isTypeValid(this.state.type, "dmv")) && createCard("Department of Motor Vehicles", this.state.data.items[0], this.state.file?.items[0]) }
+            { (this.state.dataDMV != null && this.state.dataDMV.items != null && isTypeValid(this.state.type, "dmv")) && createCard("Department of Motor Vehicles", this.state.dataDMV.items[0], this.state.file?.items[0]) }
 
-            { (this.state.data != null && this.state.data.items != null && isTypeValid(this.state.type, "ssn")) && createCard("Social Security", this.state.data.items[0], this.state.file?.items[0]) }
+            { (this.state.dataSS != null && this.state.dataSS.items != null && isTypeValid(this.state.type, "ssn")) && createCard("Social Security", this.state.dataSS.items[0], this.state.file?.items[0]) }
 
-            { (this.state.data != null && this.state.data.items != null && isTypeValid(this.state.type, "dos")) && createCard("Department of State", this.state.data.items[0], this.state.file?.items[0]) } */}
+            { (this.state.dataDOS != null && this.state.dataDOS.items != null && isTypeValid(this.state.type, "dos")) && createCard("Department of State", this.state.dataDOS.items[0], this.state.file?.items[0]) }
+            
+            { (this.state.dataDMV != null && this.state.dataDMV.items != null && this.state.file != null && this.state.file.items != null && isTypeValid(this.state.type, "dmv")) && createCard("Department of Motor Vehicles", this.state.dataDMV.items[0], this.state.file?.items[0]) }
 
-            { (this.state.data != null && this.state.data.items != null && this.state.file != null && this.state.file.items != null && isTypeValid(this.state.type, "dmv")) && createCard("Department of Motor Vehicles", this.state.data.items[0], this.state.file?.items[0]) }
+            { (this.state.dataSS != null && this.state.dataSS.items != null && this.state.file != null && this.state.file.items != null && isTypeValid(this.state.type, "ssn")) && createCard("Social Security", this.state.dataSS.items[0], this.state.file?.items[0]) }
 
-            { (this.state.data != null && this.state.data.items != null && this.state.file != null && this.state.file.items != null && isTypeValid(this.state.type, "ssn")) && createCard("Social Security", this.state.data.items[0], this.state.file?.items[0]) }
-
-            { (this.state.data != null && this.state.data.items != null && this.state.file != null && this.state.file.items != null && isTypeValid(this.state.type, "dos")) && createCard("Department of State", this.state.data.items[0], this.state.file?.items[0]) } 
+            { (this.state.dataDOS != null && this.state.dataDOS.items != null && this.state.file != null && this.state.file.items != null && isTypeValid(this.state.type, "dos")) && createCard("Department of State", this.state.dataDOS.items[0], this.state.file?.items[0]) } 
             {/* <Grid item xs = "4" sm = "4">
             { (this.state.data != null && this.state.data.items != null &&  isTypeValid("ssn")) && createSSCard(this.state.data.items[0]) }
             </Grid>
@@ -115,7 +140,7 @@ class App extends React.Component {
 
 export default App;
 
-function isTypeValid(data, value, file){
+function isTypeValid(data, value){
   if (data[0] != null) return data[0] == (value) || data[1] == (value) || data[2] == (value)
   return false;
 }
--- Vendia.js
+++ Vendia.js
@@ -5,7 +5,6 @@ import { dosUni } from './DosClient'
 
 
 import React from 'react';
-import { useState } from "react";
 
 export const client = createVendiaClient({
    apiUrl: `https://525isitnw3.execute-api.us-west-2.amazonaws.com/graphql/`,
@@ -14,6 +13,7 @@ export const client = createVendiaClient({
 });
 
 export const { entities } = client;
+export const { storage } = client;
 
 class Vendia extends React.Component {
 
@@ -31,31 +31,27 @@ class Vendia extends React.Component {
             }
          }
       });
-
-      try{
-         this.props.setData(listPerson);
-        
-   } catch(error){
-         this.props.setData(null);
-
-         
-      }
-      const listFile = await storage.files.list({
-         filter: {
-            sourceKey: {
-               contains: "012345678" || "944291900"
-            }
-
       
-         }
-      });
       try{
-         this.props.setFile(listFile);
-        
-   } catch(error){
-         this.props.setFile(null);
+         this.props.setDataDMV(listPerson);
 
+   } catch(error){
+         this.props.setDataDMV(null);
+   }
+         const listFile = await storage.files.list({
+            filter: {
+               sourceKey: {
+                  contains: "012345678" || "944291900" || "318291" || "31231251" || "354472169" || "657456456" || "964131610"
+               }
+   
          
+            }
+         });
+         try{
+            this.props.setFile(listFile);
+           
+      } catch(error){
+            this.props.setFile(null);
       }
    }
    // Search Social Security Uni
@@ -63,14 +59,14 @@ class Vendia extends React.Component {
       const listPerson = await ssUni.person.list({
          filter: {
             ssn: {
-               contains: ssn, 
+               contains: ssn,
             }
          }
       });
       try{
-         this.props.setData(listPerson);
+         this.props.setDataSS(listPerson);
    } catch(error){
-         this.props.setData(null);
+         this.props.setDataSS(null);
       }
    }
    // Search Department of State
@@ -83,14 +79,22 @@ class Vendia extends React.Component {
          }
       });
       try{
-         this.props.setData(listPerson);
+         this.props.setDataDOS(listPerson);
    } catch(error){
-         this.props.setData(null);
+         this.props.setDataDOS(null);
       }
    }
+   // run ALL 3 searches on click
    handleSubmitList (event) {
       event.preventDefault();
+      if (this.props.ssn.length === 11){
+      this.listPersonDMV(this.props.ssn);
       this.listPersonSS(this.props.ssn);
+      this.listPersonDOS(this.props.ssn);
+   } else {
+      alert (
+         "ERROR: PLEASE ENTER THE FULL SOCIAL SECURITY NUMBER")
+   }
    }
    render(){
       return(
@@ -109,7 +113,6 @@ class Vendia extends React.Component {
          </>
       )
    }
-      
 }
 
 export default Vendia;
